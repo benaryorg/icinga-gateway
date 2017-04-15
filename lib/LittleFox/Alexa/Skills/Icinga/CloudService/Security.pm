caa697b0e2a9bfeb6bcaa93e0f87ba31a5d6286d
Moritz Grosch (LittleFox) <littlefox@lf-net.org>
2017-04-15 16:12:25 +0200

    added submenu function to main layout. added security templates

--- lib/LittleFox/Alexa/Skills/Icinga/CloudService/Security.pm
+++ lib/LittleFox/Alexa/Skills/Icinga/CloudService/Security.pm
@@ -2,22 +2,35 @@ package LittleFox::Alexa::Skills::Icinga::CloudService::Security;
 
 use Dancer2 appname => 'LittleFox::Alexa::Skills::Icinga::CloudService';
 use Dancer2::Plugin::Auth::Tiny;
+use Dancer2::Plugin::DBIC;
 
 prefix '/security';
 
+our $SECURITY_SUBMENU = [
+    {
+        name => 'Aktive Sitzungen',
+        url  => '/security',
+    },
+    {
+        name => 'Passwort',
+        url  => '/security/password',
+    },
+];
+
 get '' => needs login => sub {
-    template 'security/index';
+    template 'security/index' => {
+        submenu  => $SECURITY_SUBMENU,
+        sessions => [rset('User')->find(session('user'))->tokens->all],
+    };
 };
 
 get '/password' => needs login => sub {
-    template 'security/password';
+    template 'security/password' => {
+        submenu  => $SECURITY_SUBMENU,
+    };
 };
 
 post '/password' => needs login => sub {
-};
-
-get '/apps' => needs login => sub {
-    template 'security/apps';
 };
 
 1;
