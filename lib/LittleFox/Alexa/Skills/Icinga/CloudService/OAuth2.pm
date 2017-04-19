eaea48a09ba0c9501496d3536b533a3a2272358d
Moritz Grosch (LittleFox) <littlefox@lf-net.org>
2017-04-19 18:52:11 +0200

    added description field to oauth2 grant dialog
    fixes #2

--- lib/LittleFox/Alexa/Skills/Icinga/CloudService/OAuth2.pm
+++ lib/LittleFox/Alexa/Skills/Icinga/CloudService/OAuth2.pm
@@ -3,12 +3,14 @@ package LittleFox::Alexa::Skills::Icinga::CloudService::OAuth2;
 use Dancer2 appname => 'LittleFox::Alexa::Skills::Icinga::CloudService';
 use Dancer2::Plugin::Auth::Tiny;
 use Dancer2::Plugin::DBIC;
+use DateTime;
 use URI::Encode 'uri_encode';
 
 prefix '/auth';
 
 get '' => needs login => sub {
-    my $client = params->{client_id} ? rset('OAuth2Client')->find(params->{client_id}) : undef;
+    my $client      = params->{client_id} ? rset('OAuth2Client')->find(params->{client_id}) : undef;
+    my $description = $client->name . ' - ' . DateTime->now->strftime('%Y-%m-%d %H:%M');
 
     template 'auth/grant' => {
         state         => params->{state},
@@ -17,6 +19,7 @@ get '' => needs login => sub {
         response_type => params->{response_type},
         scope         => params->{scope},
         redirect_uri  => params->{redirect_uri},
+        description   => $description,
     };
 };
 
@@ -26,14 +29,15 @@ post '' => needs login => sub {
 
     my $token = rset('OAuth2Token')->new(
         {
-            user_id   => session('user'),
-            client_id => $client->id,
+            description => params->{description},
+            user_id     => session('user'),
+            client_id   => $client->id,
         },
     )->insert;
 
     $redirect_url .= '#access_token=' . uri_encode($token->id) .
                      '&state='        . uri_encode(params->{state}) .
-                     '&token_type='   .  uri_encode('Bearer');
+                     '&token_type='   . uri_encode('Bearer');
 
     redirect $redirect_url;
 };
